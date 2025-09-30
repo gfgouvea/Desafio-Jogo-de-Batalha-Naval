3d94388310962eee8709527b5dabeccb2acc7868
Guilherme Gouvea <gfgouvea@gmail.com>
2025-09-30 01:22:29 +0000

    loops para adicionar dinamicamente os navios

--- BatalhaNaval.c
+++ BatalhaNaval.c
@@ -16,16 +16,35 @@ int main(){
     int navio2[3] = {3, 3, 3};
     int lin = 1;
     char col = 'A';
+    int coord1[3] = {4, 4, 0}; //primeiro valor é a linha onde começa o navio, o segundo é a coluna onde começa o navio e o terceiro valor é 0 para vertica e 1 para horizontal
+    int coord2[3] = {4, 6, 1};
 
-    //posicionando o primeiro navio na vertical.
+    //posicionando o primeiro navio.
     for(int i = 0; i < 3; i++){
-        tabuleiro [i+1][1] = navio1[0];
-    }
-
-    //posicionando o segundo navio na horiziontal.
-    for(int i = 0; i < 3; i++){
-        tabuleiro [6][i+6] = navio1[0];
-    }
+    	if(coord1[2] == 0){
+			tabuleiro [coord1[0]][coord1[1]] = navio1[i];
+			coord1[0]++;
+		} else if(coord1[2] == 1){
+			tabuleiro [coord1[0]][coord1[1]] = navio1[i];
+			coord1[1]++;
+		} else{
+			printf("Direção invalida!\n\n");
+			break;
+		}
+	}
+	//posicionando o segundo navio.
+	for(int i = 0; i < 3; i++){
+    	if(coord2[2] == 0){
+			tabuleiro [coord2[0]][coord2[1]] = navio2[i];
+			coord2[0]++;
+		} else if(coord2[2] == 1){
+			tabuleiro [coord2[0]][coord2[1]] = navio2[i];
+			coord2[1]++;
+		} else{
+			printf("Direção invalida!\n\n");
+			break;
+		}
+	}
     
     //impressão da matriz
     //rótulos das colunas
