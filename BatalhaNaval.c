ebc1d9294cb27a1f6645ceba754bbb96c3df6dce
Guilherme Gouvea <gfgouvea@gmail.com>
2025-09-30 14:05:55 +0000

    Adicinado inicialização da matriz com for e atualização de alguns comentarios.

--- BatalhaNaval.c
+++ BatalhaNaval.c
@@ -1,37 +1,20 @@
 #include <stdio.h>
 
 int main(){
-    int tabuleiro[10][10] = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    int tabuleiro[10][10], lin = 1, end = 0;
     int navio1[3] = {3, 3, 3};
     int navio2[3] = {3, 3, 3};
-    int lin = 1, end = 0;
-    char col = 'A';
-    int coord1[3] = {4, 4, 0}; //primeiro valor é a linha onde começa o navio, o segundo é a coluna onde começa o navio e o terceiro valor é 0 para vertica e 1 para horizontal
+    int coord1[3] = {4, 4, 0}; //primeiro valor é a linha onde começa o navio, o segundo é a coluna onde começa o navio e o terceiro valor o posicionamento. 0 para vertical e 1 para horizontal
     int coord2[3] = {3, 3, 1};
-
+    char col = 'A';
     
-    /*for(int i = 0; i <= 3; i++){
-        for(int j = -1; j <= 3; j++){
-            if(coord1[0]+i == coord2[0]+j && coord1[1] == coord2[1]+j){
-                printf("coord1[0]+i: %d\n", coord1[0]+i);
-                printf("coord2[0]+j: %d\n", coord2[0]+j);
-                printf("coord1[1]+i: %d\n", coord1[1]+i);
-                printf("coord2[1]+j: %d\n", coord2[1]+j); 
-                printf("ATENÇÃO!! Os navios estão se sobrepondo.\n\n");
+    //inicialização da matriz tabuleiro
+    for (int i = 0; i < 10; i++){
+            for (int j = 0; j < 10; j++){
+                tabuleiro[i][j] = 0;
+            }
         }
-        }
-    }*/
-   
+
     //posicionando o primeiro navio.
     for(int i = 0; i < 3; i++){
     	if(coord1[2] == 0){
@@ -49,7 +32,7 @@ int main(){
 	for(int i = 0; i < 3; i++){
         //executa esse bloco caso o navio for ser inserido na vertical.
     	if(coord2[2] == 0){
-            //se a coordenada já estiver ocupada (diferente de zero) alerta que os navios estão sobrepondo-se e encerra o loop.
+            //se a coordenada já estiver ocupada (diferente de zero) alerta que os navios estão sobrepondo-se, encerra o loop e muda a variável de controle end para 1.
             if(tabuleiro[coord2[0]][coord2[1]] !=0){
                 printf("ATENÇÃO!! Os navios estão se sobrepondo.\n\n");
                 break;
@@ -59,7 +42,7 @@ int main(){
             }		
         //executa esse bloco caso o navio for ser inserido na horizontal.    	
 		} else if(coord2[2] == 1){
-            //se a coordenada já estiver ocupada (diferente de zero) alerta que os navios estão sobrepondo-se e encerra o loop e muda a variável de controle para 1.
+            //se a coordenada já estiver ocupada (diferente de zero) alerta que os navios estão sobrepondo-se, encerra o loop e muda a variável de controle end para 1.
             if(tabuleiro[coord2[0]][coord2[1]] != 0){
                 printf("ATENÇÃO!! Os navios estão se sobrepondo.\n\n");
                 end = 1;
@@ -68,6 +51,7 @@ int main(){
                 tabuleiro[coord2[0]][coord2[1]] = navio2[i];
 			    coord2[1]++;
             }
+        //este bloco é executado somente quando os elementos da posição 2 das variáveis coord1 e coord2 não forem nem 0 (vertical) e nem 1 (horizontal).
 		} else{
 			printf("Direção invalida!\n\n");
             end = 1;
@@ -77,6 +61,8 @@ int main(){
     
     //impressão da matriz
     //rótulos das colunas
+    //nas linhas 40 e 50 se o condicional foi verdadeiro a variável de controle end foi setada para 1
+    //dessa forma a matriz preenchida não será exibida. Em vez disso mostra a mensagem da linha 89.
     if(end == 0){
         printf("  ");
         for(int i = 0; i < 10; i++){
